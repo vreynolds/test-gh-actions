800772114a356f9a2ed0c805577d0c8a3eb29733
Vera Reynolds <verareynolds@honeycomb.io>
2021-12-27 16:12:59 -0700

    setup test action

---
+++ .github/actions/re-triage/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+void async function () {
+	try {
+        console.log(github.context)
+		// const {eventName, payload} = github.context;
+		// const request = projects.createRequest(eventName, payload);
+		// const accessToken = core.getInput('ghprojects-token');
+		// const octokit = github.getOctokit(accessToken);
+		// await octokit.rest.projects.createCard(request);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}();
