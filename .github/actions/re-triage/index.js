a7cfaeba72067e97e2cd8f17fdcd1a203130e700
Vera Reynolds <verareynolds@honeycomb.io>
2021-12-27 17:17:28 -0700

    bigger json

--- .github/actions/re-triage/index.js
+++ .github/actions/re-triage/index.js
@@ -22,7 +22,7 @@ void async function () {
 				per_page: 100,
 				page: page
 			});
-			console.log("CARDS: " + json.stringify(cards));
+			console.log("CARDS: " + JSON.stringify(cards));
 			if (cards.length == 0) {
 				break;
 			}
