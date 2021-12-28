aae3c843e28029ae778284abfe0afb3ade532546
Vera Reynolds <verareynolds@honeycomb.io>
2021-12-27 17:11:51 -0700

    attempt to find the issue card

--- .github/actions/re-triage/index.js
+++ .github/actions/re-triage/index.js
@@ -5,11 +5,34 @@ const github = require('@actions/github');
 
 void async function () {
 	try {
-        console.log(github.context.payload.issue.url)
+        const issueUrl = github.context.payload.issue.url;
+		// project - 13957392
+		// triawge - 17334783
+		// ice - 17334784
 		// const {eventName, payload} = github.context;
 		// const request = projects.createRequest(eventName, payload);
-		// const accessToken = core.getInput('ghprojects-token');
-		// const octokit = github.getOctokit(accessToken);
+		const accessToken = core.getInput('ghprojects-token');
+		const octokit = github.getOctokit(accessToken);
+		let page = 1;
+		let issueCard;
+		while (!issueCard) {
+			let cards = await octokit.rest.projects.listCards({
+				column_id: 17334784,
+				archived_state: "not_archived",
+				per_page: 100,
+				page: page
+			});
+			if (cards.length == 0) {
+				break;
+			}
+			let matches = cards.filter(card => card.content_url === issueUrl);
+			if(matches.length > 0) {
+				issueCard = matches[0];
+			} else {
+				page++;
+			}
+		}
+		console.log('founds: ' + issueCard)
 		// await octokit.rest.projects.createCard(request);
 	} catch (error) {
 		core.setFailed(error.message);
